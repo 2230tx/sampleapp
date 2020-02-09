b0212900f48318bc42c3a81742a2edbb71b33569
Abhay <Abhay@DESKTOP-APST>
2020-02-09 15:18:35 +0530

    1st test commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	private static int id;
+
+	public static int getId() {
+		return id;
+	}
+
+	public static void setId(int id) {
+		Test.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Test []";
+	}
+
+	
+}
